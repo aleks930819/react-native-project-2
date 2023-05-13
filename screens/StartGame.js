ddb336fd7fe28227543bc4d361c80065d403ce41
Aleksandar Atanasov <aleks930819@gmail.com>
2023-05-13 12:23:28 +0300

    added buttons box

--- screens/StartGame.js
+++ screens/StartGame.js
@@ -1,11 +1,29 @@
-import { View, TextInput } from 'react-native';
+import { View, TextInput, StyleSheet } from 'react-native';
+import PrimaryButton from '../components/PrimaryButton';
 
 const StartGame = () => {
   return (
-    <View>
+    <View style={styles.inputContainer}>
       <TextInput />
+      <PrimaryButton>Reset</PrimaryButton>
+      <PrimaryButton>Confirm</PrimaryButton>
     </View>
   );
 };
 
 export default StartGame;
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    padding: 10,
+    marginTop: 100,
+    marginHorizontal: 24,
+    backgroundColor: '#72063a',
+    borderRadius: 10,
+    elevation: 5,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.5,
+  },
+});
